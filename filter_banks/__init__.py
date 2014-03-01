34072734f58afc9a881aac8b50f027cdc954be63
Marc Joliet <marcec@gmx.de>
2014-03-01 14:20:24 +0100

    Minor stylistic change to the package docstring

--- filter_banks/__init__.py
+++ filter_banks/__init__.py
@@ -1,12 +1,7 @@
 """This package contains functions and classes that implement various forms of
 filter banks.
 
-Submodules
------------
-
-This package contains the following sub-modules:
-
-::
+This package contains the following modules::
 
     regalia_mitra -- Regalia-Mitra doubly-complementary filter bank
 """
